465aef802d890fde00d421a88b71a3e7d67e6739
Afnan Mirza <afnanmirza1999@gmail.com>
2020-10-07 19:59:54 -0700

    1. Integrated Swift Package Manager.

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "HGPlaceholders",
+    products: [
+        .library(
+            name: "HGPlaceholders",
+            targets: ["HGPlaceholders"]),
+    ],
+    targets: [
+        .target(
+            name: "HGPlaceholders",
+            dependencies: []),
+    ]
+)
